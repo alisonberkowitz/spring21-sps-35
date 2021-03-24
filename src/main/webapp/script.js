3442dc9d83e3dc30bd73a8f48b6f1a716bb02e72
Sterly Deracy <SDeracy@gmail.com>
2021-03-24 00:49:00 +0000

    Add images for buttons, create student html page for student view

--- src/main/webapp/script.js
+++ src/main/webapp/script.js
@@ -26,3 +26,7 @@ function addRandomGreeting() {
   const greetingContainer = document.getElementById('greeting-container');
   greetingContainer.innerText = greeting;
 }
+
+function loadStudent() {
+    window.location.href = "student.html";
+}
